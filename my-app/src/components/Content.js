4e4f35bbfa322b964f00b9ee26fda1a8b1e2bc9f
dEEGE2time <johannesweidensjo@hotmail.com>
2023-07-24 23:02:40 +0000

    filter search

---
+++ my-app/src/components/Content.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import css from "./css/Content.module.css";
+import {savedPosts} from "../posts.json";
+import PostItem from "./PostItem";
+import Loader from "./Loader";
+
+export class Content extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoaded: false,
+            posts: [],
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+                posts: savedPosts
+            })
+        }, 2000)
+    }
+
+
+    handleChange = (event) => {
+        const name = event.target.value.toLowerCase()
+        const filteredPosts = savedPosts.filter((post) => {
+            return post.name.toLowerCase().includes(name)
+        })
+
+        this.setState({
+            posts: filteredPosts,
+        })
+    }
+
+    
+    render() {
+        return (
+            <div className={css.Content}>
+                
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor="searchInput">Search:</label>
+                        <input
+                          type='text'
+                          id='searchInput'
+                          onChange={(event) => {
+                            this.handleChange(event)
+                          }}
+                        />
+                        <h4>Posts found: {this.state.posts.length}</h4>
+                    </form>
+                </div>
+
+                <div className={css.SearchResults}>
+                    {
+                        this.state.isLoaded ? 
+                        <PostItem savedPosts={this.state.posts}/>
+                        : <Loader />
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Content
